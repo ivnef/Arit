8c823f7d1ee9814be8da601910a21fc35af8b63e
Igor Nefedov <ivnef@rambler.ru>
2021-05-30 11:20:02 +0300

    Create class the Arithmetic in the 03_ObjectsAndMethods project

---
+++ src/Arithmetic.java
@@ -0,0 +1,26 @@
+
+
+
+public class Arithmetic {
+public Arithmetic (int x,int y) {
+    System.out.println( "Арифметические действия: "   );
+    System.out.println("Сумма: " + (x + y));
+    System.out.println("Разность: " + (x - y));
+    System.out.println("Произведение: " + (x * y));
+    System.out.println("Среднее знвчение чисел: " + ((x + y)/2));
+    System.out.println("Максимальное из чисел: " + Math.max(x, y));
+    System.out.println("Минимальное из чисел: " + Math.min(x, y));
+
+}
+
+public static void main (String[] args) {
+
+    Arithmetic number = new Arithmetic(70,50);
+
+}
+
+        }
+
+
+
+
