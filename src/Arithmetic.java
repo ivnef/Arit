e89bc48d0a806bc7b1b217188428a5cc3b237b3c
Igor Nefedov <ivnef@rambler.ru>
2021-06-27 19:34:13 +0300

    New create a class the Arithmetic in the 03_ObjectsAndMethods project _1

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -2,24 +2,73 @@
 
 
 public class Arithmetic {
-public Arithmetic (int x,int y) {
-    System.out.println( "Арифметические действия: "   );
-    System.out.println("Сумма: " + (x + y));
-    System.out.println("Разность: " + (x - y));
-    System.out.println("Произведение: " + (x * y));
-    System.out.println("Среднее знвчение чисел: " + ((x + y)/2));
-    System.out.println("Максимальное из чисел: " + Math.max(x, y));
-    System.out.println("Минимальное из чисел: " + Math.min(x, y));
+   private int a;
+   private int b;
 
+    public void getSum () {
+        int sum = (a + b);
+        System.out.println(sum);
+    }
+    public void getDifference() {
+        int difference = (a - b);
+        System.out.println(difference);
+    }
+    public void getProductOfNumbers () {
+        int product = (a * b);
+        System.out.println(product);
+    }
+
+    public void getAverageOfNumbers() {
+        int average = ((a + b) / 2);
+        System.out.println(average);
+    }
+    public void getMaximumOfNumbers () {
+
+        int maxOfNumbers = Math.max(a, b);
+        System.out.println(maxOfNumbers);
+    }
+
+    public void getMinimumOfNumbers () {
+
+        int minOfNumbers = Math.min(a, b);
+        System.out.println(minOfNumbers);
+    }
+
+    public Arithmetic (int a, int b) {
+
+        this.a = a;
+        this.b = b;
 }
 
-public static void main (String[] args) {
+    public static void main(String[] args) {
 
-    Arithmetic number = new Arithmetic(70,50);
+        Arithmetic numbers = new Arithmetic(70,50);
+        System.out.println("Арифметические действия: ");
 
+        System.out.print( " Сумма чисел :" );
+        numbers.getSum();
+
+        System.out.print( " Разность чисел :" );
+        numbers.getDifference();
+
+        System.out.print( " Произведение чисел :" );
+        numbers.getProductOfNumbers();
+
+        System.out.println("Среднее значение чисел: ");
+        numbers.getAverageOfNumbers();
+
+        System.out.println("Максимальное из чисел: ");
+        numbers.getMaximumOfNumbers ();
+
+        System.out.println("Минимальное из чисел: ");
+        numbers.getMinimumOfNumbers ();
+
+
+
+    }
 }
 
-        }
+
 
 
 
